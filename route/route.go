66fecd6f6203232facc6b3f1411b3e30b2ecb43e
SouthOrange <15280970040@163.com>
2021-02-19 18:08:57 +0800

    chore: 中间件拆分

--- route/route.go
+++ route/route.go
@@ -2,31 +2,19 @@ package route
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/unrolled/secure"
 	"go_web_starter/config"
 	"go_web_starter/controller"
+	"go_web_starter/middleware"
 )
 
 var Application *gin.Engine
 
-func TlsHandler() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		secureMiddleware := secure.New(secure.Options{
-			SSLRedirect: true,
-			SSLHost:     ":443",
-		})
-		err := secureMiddleware.Process(c.Writer, c.Request)
-		if err != nil {
-			return
-		}
-		c.Next()
-	}
-}
+
 
 func Init() {
 	router := gin.New()
 	if config.Config.AppConfig.Https == true {
-		router.Use(TlsHandler())
+		router.Use(middleware.TlsHandler())
 	}
 
 	Application = router
